24db129c5183dd1c56f498ce7992c3815e4f86b0
Giovanni Maria Cusaro <gcusaro@me.com>
2021-09-19 09:07:39 +0200

    Nvim init config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+--[[
+
+Neovim init file
+
+Author: Giovanni Maria Cusaro
+Source: https://github.com/gmcusaro/dotfiles
+
+--]]
+
+
+require('setup')
+require('plugin')
+require('mapping')
+require('settings')
