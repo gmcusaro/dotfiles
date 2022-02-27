15a3ce95173c0ccd36794f421fbc29a9e12f2b2e
Giovanni Maria Cusaro <gcusaro@me.com>
2022-02-27 17:23:10 +0100

    updated some options

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,18 +4,17 @@ local actions = require'telescope.actions'
 require'telescope'.setup {
   defaults = {
     file_sorter = require("telescope.sorters").get_fzy_sorter,
-    prompt_prefix = " >",
+    prompt_prefix = "  ",
     color_devicons = true,
-
+    initial_mode = "insert",
     previewer = true,
     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
     grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
     mappings = {
       i = {
-        ["<C-j>"] = actions.move_selection_better,
-        ["<C-k>"] = actions.move_selection_worse,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
         ["<Esc>"] = actions.close
       }
     }
