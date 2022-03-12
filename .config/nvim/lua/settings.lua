9fc6bdff8dd504f3df1c3337937b80f844d3064e
Giovanni Maria Cusaro <gcusaro@me.com>
2022-03-12 21:52:12 +0100

    updated settings

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -10,27 +10,20 @@ local o    = vim.opt              -- global/buffer/windows-scoped options
 g.modeline=0
 g.scriptencoding='utf-8'
 
--- Don't auto commenting new lines
-cmd[[au BufEnter * set fo-=c fo-=r fo-=o]]
-
--- Remove whitespace on save
-cmd[[au BufWritePre * :%s/\s\+$//e]]
-
--- Auto save files when focus is lost
-cmd[[au FocusLost, WinLeave * :silent! noautocmd w]]
-
--- Set color column magenta
-cmd[[highlight ColorColumn ctermbg=magenta]]
+cmd[[au BufEnter * set fo-=c fo-=r fo-=o]] -- Don't auto commenting new lines
+cmd[[au BufWritePre * :%s/\s\+$//e]] -- Remove whitespace on save
+cmd[[au FocusLost, WinLeave * :silent! noautocmd w]] -- Auto save files when focus is lost
+cmd[[highlight ColorColumn ctermbg=magenta]] -- Set color column magenta
 
 -- Disable backup and swap files
 g.nobackup = true
 g.nowritebackup = true
 g.noswapfile = true
 
--- Do not show the mode in the command line
 g.noshowmode = false
 o.completeopt = 'menu,menuone,noselect,noinsert'
 o.pumblend = 18
+o.wildmode = 'longest:full'
 
 -- Eliminate delays
 o.timeout = false
@@ -38,10 +31,8 @@ o.timeoutlen = 0
 o.ttimeout = false
 o.ttimeoutlen = 0
 
--- Enable mouse support
-o.mouse = "nv"
-o.cmdheight = 2
-o.scrolloff = 7
+o.mouse = "nv" -- Enable mouse support
+o.scrolloff = 10 -- Make it so there are always ten lines below my cursor
 
 -- Set tab behavior
 o.tabstop = 2
@@ -52,25 +43,19 @@ o.expandtab = true
 o.confirm = true
 o.smartcase = true
 
--- Split direction
-o.splitbelow = true
-o.splitright = true
+o.splitbelow = true -- Split direction below
+o.splitright = true -- Split direction right
+o.equalalways = false -- Not resize other windows
 
 o.showmatch = true
-o.ignorecase = true
+o.ignorecase = true -- Ignore case when searching
 
 o.number = true
 o.breakindent = true
 
--- Show whitespace characters
-o.list = true
+o.list = true -- Show whitespace characters
+o.clipboard = "unnamedplus" -- Use the system clipboard
+o.shortmess:append("I") -- Do not show the startup message
 
--- Use the system clipboard
-o.clipboard = "unnamedplus"
-
--- Do not show the startup message
-o.shortmess:append("I")
-
--- Theme
 o.termguicolors = true
-cmd 'colorscheme nightfox'
+cmd 'colorscheme nightfox' -- Theme
