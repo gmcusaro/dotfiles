3e3403b53e6e119d7ebee83c4ddb85aefeb67b9b
Giovanni Maria Cusaro <gcusaro@me.com>
2022-03-12 21:54:25 +0100

    edited header

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,13 @@
 --[[
 
-Neovim init file
+   ____ _ ____ ___   _____
+  / __ `// __ `__ \ / ___/
+ / /_/ // / / / / // /__
+ \__, //_/ /_/ /_/ \___/
+/____/
+
+
+Neovim lua init file
 Author: Giovanni Maria Cusaro
 Source: https://github.com/gmcusaro/dotfiles
 
