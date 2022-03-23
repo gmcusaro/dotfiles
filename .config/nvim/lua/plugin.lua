367552d5d0fc56c1f31f65786afbb22902969003
Giovanni Maria Cusaro <gcusaro@me.com>
2022-03-23 10:46:28 +0100

    auto update Packer.nvim

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -1,5 +1,21 @@
+vim.cmd([[
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
+]])
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+-- Load packer.nvim
+vim.cmd("packadd packer.nvim")
+
+-- Load packer.nvim
 require('packer').startup(function(use)
--- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
   use 'machakann/vim-sandwich'
@@ -60,4 +76,9 @@ require('packer').startup(function(use)
   -- LSP
   use 'neovim/nvim-lspconfig'
   use 'j-hui/fidget.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
