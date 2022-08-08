263b0c4bdf570d8d2060592959e732075de1596a
Giovanni Maria Cusaro <gcusaro@me.com>
2022-08-08 08:58:48 +0200

    clean autocomands

--- .config/nvim/after/plugin/autocomands.lua
+++ .config/nvim/after/plugin/autocomands.lua
@@ -5,5 +5,15 @@ utils.define_augroups({
         {'BufWinEnter', '*', 'setlocal formatoptions-=r formatoptions-=o'},
         {'BufRead', '*', 'setlocal formatoptions-=r formatoptions-=o'},
         {'BufNewFile', '*', 'setlocal formatoptions-=r formatoptions-=o'},
+        {'FocusLost, WinLeave', '*', 'wall'},
+        {'BufWritePre', '*', '%s/\\s\\+$//e'},
+        {'BufReadPre', '*', 'filetype plugin indent on'},
+        {'BufWritePost', 'plugin.lua', 'source <afile> | PackerCompile'}
     },
 })
+
+-- autocmd({"BufReadPre"}, {
+--     pattern = {'*'},
+--     command = [[call matchadd('ColorColumn', '\%81v\s*\zs\S', 100)]]
+-- })
+
