82bb52009ece56e406bd7bce3d4a91c840dcfbb4
Giovanni Maria Cusaro <gio@MacBook.local>
2022-08-11 17:42:41 +0200

    added neogit support

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,16 +1,39 @@
+local neogit_extension = {
+  filetypes = {'NeogitStatus', 'NeogitCommitMessage', 'NeogitPopup', 'NeogitLogView', 'NeogitGitCommandHistory'}
+}
+
 require('lualine').setup  {
     options = {
         section_separators = { left = '', right = '' },
         component_separators = '',
         theme = "rose-pine",
-        icons_enabled = true
+        icons_enabled = true,
+        disabled_filetypes = { 'NeogitStatus', 'Packer' },
     },
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff' },
-        lualine_c = { 'buffers' },
+        lualine_c = { {
+            'filename',
+            file_status = true,
+            path = 0,
+        } },
         lualine_x = { },
         lualine_y = { 'filetype', 'encoding', 'location' },
         lualine_z = { }
-    }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {{
+            'filename',
+            file_status = true,
+            path = 1
+        }},
+        lualine_x = { },
+        lualine_y = { 'location' },
+        lualine_z = { }
+    },
+    tabline = { },
+    extensions = { neogit_extension }
 }
