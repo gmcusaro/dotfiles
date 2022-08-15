5bfebc14ef96a13a967cf219b2f1cec81e5b2561
Giovanni Maria Cusaro <gcusaro@me.com>
2022-08-15 10:52:32 +0200

    fix spaces

--- .config/nvim/lua/gio/packer/init.lua
+++ .config/nvim/lua/gio/packer/init.lua
@@ -8,7 +8,6 @@ end
 
 require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-
     use {
         'rose-pine/neovim',
         as = 'rose-pine',
@@ -17,15 +16,13 @@ require('packer').startup(function(use)
             vim.cmd('colorscheme rose-pine')
         end
     }
-
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-
     use {'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim'}
-
     use {'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim'}
-
-    use {'hoob3rt/lualine.nvim', require = 'kyazdani42/nvim-web-devicons', opt = true}
-
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
     use 'neovim/nvim-lspconfig'
     use 'onsails/lspkind-nvim'
     use 'hrsh7th/cmp-buffer'
@@ -33,17 +30,13 @@ require('packer').startup(function(use)
     use 'hrsh7th/nvim-cmp'
 
     use 'L3MON4D3/LuaSnip'
-
     use 'ur4ltz/surround.nvim'
-
     use {'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
         end
     }
-
-    use {'jdhao/better-escape.vim', event = 'InsertEnter'}
-
+    use {'jdhao/better-escape.vim', event = 'InsertEnter*'}
     use 'Asheq/close-buffers.vim'
 
     if packer_bootstrap then
