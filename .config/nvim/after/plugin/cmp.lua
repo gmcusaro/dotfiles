1175166009d784a373a5586d0b90a6690229cb4c
Giovanni Maria Cusaro <gcusaro@me.com>
2022-08-22 09:45:36 +0200

    small improvements

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -1,27 +1,46 @@
 local cmp = require('cmp')
 local lspkind = require('lspkind')
+local luasnip = require('luasnip')
 
 cmp.setup({
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
         end
     },
     mapping = cmp.mapping.preset.insert({
-      ['<leader>k'] = cmp.mapping.scroll_docs(-5),
-      ['<leader>j'] = cmp.mapping.scroll_docs(5),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({
-          select = true,
+        ['<leader>k'] = cmp.mapping.scroll_docs(-5),
+        ['<leader>j'] = cmp.mapping.scroll_docs(5),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
-      }),
-      ['<C-k>'] = cmp.mapping.select_prev_item(),
-      ['<C-j>'] = cmp.mapping.select_next_item(),
+          select = true,
+        }),
+        -- ['<C-k>'] = cmp.mapping.select_prev_item(),
+        -- ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        -- { name = 'luasnip' },
+        { name = 'luasnip' },
         { name = 'buffer' }
     }),
     formatting = {
