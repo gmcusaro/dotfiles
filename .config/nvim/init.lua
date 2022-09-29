e35876e947b662b9bc7f3f0edd4c9315eabd117b
Giovanni Maria Cusaro <gcusaro@me.com>
2022-09-29 14:23:51 +0200

    added banner

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,10 @@
+--     .88888.  8888ba.88ba   a88888b.
+--    d8'   `88 88  `8b  `8b d8'   `88
+--    88        88   88   88 88
+--    88   YP88 88   88   88 88
+--    Y8.   .88 88   88   88 Y8.   .88
+--     `88888'  dP   dP   dP  Y88888P'
+
+-- Warning: Don't use this settings unless you know what that entails
+
 require("gio")
