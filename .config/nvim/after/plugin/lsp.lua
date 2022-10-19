6c6a21d682d243fe30bbf7f4856f01284f9a6c83
gio <gio@MacBook.local>
2022-10-19 17:34:54 +0200

    update update_capabilities deprecated

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -38,7 +38,7 @@ local settings = {
     },
 }
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup({
         capabilities = capabilities,
