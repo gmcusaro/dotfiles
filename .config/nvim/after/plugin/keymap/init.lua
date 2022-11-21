179a7552a09d73ce309dd5d0b529bbc3454f2bed
Giovanni Maria Cusaro <gcusaro@me.com>
2022-11-21 20:37:24 +0100

    refactor silent option

--- .config/nvim/after/plugin/keymap/init.lua
+++ .config/nvim/after/plugin/keymap/init.lua
@@ -5,14 +5,16 @@ local vnoremap = Remap.vnoremap
 -- local xnoremap = Remap.xnoremap
 -- local nmap     = Remap.nmap
 
+local silent = { silent = true }
+
 nnoremap('x', '"_x') -- Do not yank with x
 nnoremap('<leader>a', 'gg<S-v>G') -- Select all
 
 vnoremap('<BS>', '\"_d') -- Backspace delete visual mode
-vnoremap('K', ":m '<-2<CR>gv=gv", { silent = true }) -- Move UP
-vnoremap('J', ":m '>+1<CR>gv=gv", { silent = true }) -- Move DOWN
+vnoremap('K', ":m '<-2<CR>gv=gv", silent) -- Move UP
+vnoremap('J', ":m '>+1<CR>gv=gv", silent) -- Move DOWN
 
-nnoremap('Q', ':noh<CR>', { silent = true })
+nnoremap('Q', ':noh<CR>', silent)
 
 -- Navigation - Split - Resize
 nnoremap('sh', '<C-w>h')
@@ -20,15 +22,15 @@ nnoremap('sj', '<C-w>j')
 nnoremap('sk', '<C-w>k')
 nnoremap('sl', '<C-w>l')
 
-nnoremap('<leader>v', ':vsplit<CR>', { silent = true })
-nnoremap('<leader>h', ':split<CR>', { silent = true })
-nnoremap('<leader>+', ':vertical resize +5<CR>', { silent = true })
-nnoremap('<leader>-', ':vertical resize -5<CR>', { silent = true })
+nnoremap('<leader>v', ':vsplit<CR>', silent)
+nnoremap('<leader>h', ':split<CR>', silent)
+nnoremap('<leader>+', ':vertical resize +5<CR>', silent)
+nnoremap('<leader>-', ':vertical resize -5<CR>', silent)
 
 -- Tab navigation
-nnoremap('<TAB>',   ':bnext<CR>', { silent = true })
-nnoremap('<S-TAB>', ':bprevious<CR>', { silent = true })
+nnoremap('<TAB>',   ':bnext<CR>', silent)
+nnoremap('<S-TAB>', ':bprevious<CR>', silent)
 
 -- Buffer delete
-nnoremap('<leader>b', ':Bdelete menu<CR>', { silent = true })
-nnoremap('<leader>t', ':Bdelete this<CR>', { silent = true })
+nnoremap('<leader>b', ':Bdelete menu<CR>', silent)
+nnoremap('<leader>t', ':Bdelete this<CR>', silent)
