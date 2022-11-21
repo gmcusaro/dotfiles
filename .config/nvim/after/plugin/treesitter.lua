15a4b9de67c81e440685dfa9191718d2b1070f9e
Giovanni Maria Cusaro <gcusaro@me.com>
2022-11-21 20:38:11 +0100

    clean code

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     sync_install = true,
     auto_install = true,
-    -- ignore_install = { "" },
+    ignore_install = {},
     ensure_installed = {
         'c',
         'lua',
