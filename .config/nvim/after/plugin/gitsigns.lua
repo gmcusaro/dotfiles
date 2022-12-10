2eb1d1ed4370598c60498fc33ef1c22557ab6e26
Giovanni Maria Cusaro <gcusaro@me.com>
2022-12-10 18:26:46 +0100

    update signs

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,24 +1,12 @@
 require('gitsigns').setup({
     signs = {
-        add          = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" }, --┃
-    --     change       = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    --     delete       = { hl = "GitSignsDelete", text = "━", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    --     topdelete    = { hl = "GitSignsDelete", text = "━", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    --     changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        add          = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        untracked    = { hl = 'GitSignsAdd', text = '¦', numhl='GitSignsAddNr', linehl='GitSignsAddLn' },
     },
     current_line_blame_formatter = '<summary> | <author_time:%d-%m-%Y> - <author>',
     current_line_blame_opts = {
         virt_text_pos = 'right_align'
     },
-    -- TODO check options
-    -- preview_config = {
-    --     -- Options passed to nvim_open_win
-    --     border = 'double',
-    --     -- style = 'minimal',
-    --     -- relative = 'cursor',
-    --     row = 3,
-    --     col = 3
-    -- },
 })
 
 local keymap = require("gio.keymaps")
