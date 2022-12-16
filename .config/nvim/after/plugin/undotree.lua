68ee84f55178c5c5865a7b7cd6c3f161507422e6
Giovanni Maria Cusaro <gcusaro@me.com>
2022-12-16 11:23:36 +0100

    set undotree opt

--- .config/nvim/after/plugin/undotree.lua
+++ .config/nvim/after/plugin/undotree.lua
@@ -1,1 +1,10 @@
-require('undotree').setup()
+local undotree = require('undotree')
+
+undotree.setup({
+    float_diff = false
+})
+
+local keymaps = require("gio.keymaps")
+local nnoremap = keymaps.nnoremap
+
+nnoremap('<leader>u', undotree.toggle, keymaps.opts)
