20696fffde5ab5152402309e64d89afc05a28483
Giovanni Maria Cusaro <gcusaro@me.com>
2022-12-27 09:17:08 +0100

    fix typo

--- .config/nvim/lua/gio/keymaps/init.lua
+++ .config/nvim/lua/gio/keymaps/init.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+-- make reafact https://github.com/toddky/nvim/blob/8f0f38aae8fc31a3524c51fbfd42e5166066d75a/lua/util/init.lua
+
 function Bind(op, outer_opts)
     outer_opts = outer_opts or { noremap = true }
     return function(lhs, rhs, opts)
