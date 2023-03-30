4b0b3916b9703d71439b3d0d2c1b61c63be089d5
Giovanni Maria Cusaro <gcusaro@me.com>
2023-03-30 17:11:41 +0200

    replace single quote with double

--- .config/nvim/lua/gio/plugins.lua
+++ .config/nvim/lua/gio/plugins.lua
@@ -12,48 +12,47 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-    { 'nvim-lua/plenary.nvim' }, -- Common Utilities
+    { "nvim-lua/plenary.nvim" }, -- Common Utilities
 
-    { 'catppuccin/nvim', name = 'catppuccin' },
+    { "catppuccin/nvim", name = "catppuccin" },
 
-    { 'TimUntersberger/neogit' },
-    { 'lewis6991/gitsigns.nvim' },
-    { 'sindrets/diffview.nvim' },
-    { 'jiaoshijie/undotree' },
+    { "TimUntersberger/neogit" },
+    { "lewis6991/gitsigns.nvim" },
+    { "sindrets/diffview.nvim" },
+    { "jiaoshijie/undotree" },
 
-    { 'nvim-lualine/lualine.nvim' },
+    { "nvim-lualine/lualine.nvim" },
 
-    { 'nvim-treesitter/nvim-treesitter', build = { ':TSUpdate' } },
-    { 'nvim-telescope/telescope.nvim' },
-    { 'nvim-telescope/telescope-file-browser.nvim' },
-    { 'nvim-telescope/telescope-live-grep-args.nvim' },
+    { "nvim-treesitter/nvim-treesitter", build = { ":TSUpdate" } },
+    { "nvim-telescope/telescope.nvim" },
+    { "nvim-telescope/telescope-file-browser.nvim" },
+    { "nvim-telescope/telescope-live-grep-args.nvim" },
     { "ThePrimeagen/refactoring.nvim" },
 
-    'ur4ltz/surround.nvim',
-    'numToStr/Comment.nvim',
-    'windwp/nvim-ts-autotag',
-    'windwp/nvim-autopairs',
-    'Asheq/close-buffers.vim',
-    { 'jdhao/better-escape.vim', event = { 'InsertEnter' } },
+    "ur4ltz/surround.nvim",
+    "numToStr/Comment.nvim",
+    "windwp/nvim-ts-autotag",
+    "windwp/nvim-autopairs",
+    "Asheq/close-buffers.vim",
+    { "jdhao/better-escape.vim", event = { "InsertEnter" } },
 
-    {   'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
+    {   "VonHeikemen/lsp-zero.nvim",
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+            {"neovim/nvim-lspconfig"},
+            {"williamboman/mason.nvim"},
+            {"williamboman/mason-lspconfig.nvim"},
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lua'},
+            {"hrsh7th/nvim-cmp"},
+            {"hrsh7th/cmp-nvim-lsp"},
+            {"hrsh7th/cmp-buffer"},
+            {"hrsh7th/cmp-path"},
+            {"saadparwaiz1/cmp_luasnip"},
+            {"hrsh7th/cmp-nvim-lua"},
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},
+            {"L3MON4D3/LuaSnip"},
         }
     }
 })
