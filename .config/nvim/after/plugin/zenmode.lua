d52d7cebfa25e4470d95467ed5ccd5aca291e64c
Giovanni Maria Cusaro <gcusaro@me.com>
2023-05-14 21:46:58 +0200

    set zenmode options

---
+++ .config/nvim/after/plugin/zenmode.lua
@@ -0,0 +1,10 @@
+require("zen-mode").setup({
+    window = {
+        width = .80,
+        height = 0.9,
+        backdrop = 1,
+        options = {
+            cursorline = false
+        }
+    }
+})
