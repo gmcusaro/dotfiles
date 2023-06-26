1f8a57bce3b2b15ce6b67e0ae37ec7b1f53c54d1
Giovanni Maria Cusaro <gcusaro@me.com>
2023-06-26 09:43:09 +0200

    plugin md preview

--- .config/nvim/lua/gio/plugins.lua
+++ .config/nvim/lua/gio/plugins.lua
@@ -38,6 +38,13 @@ require('lazy').setup({
     { "windwp/nvim-autopairs" },
     { "Asheq/close-buffers.vim" },
     -- { "ThePrimeagen/refactoring.nvim" },
+    { "iamcco/markdown-preview.nvim",
+      build = "cd app && npm install",
+      init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+      end,
+      ft = { "markdown" }},
+-- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
     { "jdhao/better-escape.vim", event = { "InsertEnter" } },
 
