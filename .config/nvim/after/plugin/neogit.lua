3227b3d44c519f0401ac509e491859dad0a53666
Giovanni Maria Cusaro <gcusaro@me.com>
2023-07-19 14:27:56 +0200

    clean settings

--- .config/nvim/after/plugin/neogit.lua
+++ .config/nvim/after/plugin/neogit.lua
@@ -1,30 +1,22 @@
 local neogit = require('neogit')
 
 neogit.setup {
-    commit_popup = {
-        kind = "split_above"
-    },
     integrations = {
         diffview = true
     },
     disable_commit_confirmation = true,
 }
 
+local opts = { silent = true, noremap = true }
+
 local function opencommit()
     neogit.open({ 'commit' })
 end
 
 local function opensplit()
-    neogit.open({ kind = "split_above" })
+    neogit.open({ kind = "split" })
 end
 
-local opts = { silent = true, noremap = true }
-
 vim.keymap.set("n", "<leader>gc", opencommit, opts)
--- nnoremap('<leader>gc', opencommit, keymap.opts)
-
 vim.keymap.set("n", "<leader>gs", opensplit, opts)
--- nnoremap('<leader>gs', opensplit, keymap.opts)
-
 vim.keymap.set("n", "<leader>gf", "<cmd>!git fetch --all<CR>", opts)
--- nnoremap("<leader>gf", "<cmd>!git fetch --all<cr>", keymap.opts)
