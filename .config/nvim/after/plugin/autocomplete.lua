536eaeee1becf3145378405070d217f5915a843b
Giovanni Maria Cusaro <gcusaro@me.com>
2023-10-04 21:35:56 +0200

    update preset

--- .config/nvim/after/plugin/autocomplete.lua
+++ .config/nvim/after/plugin/autocomplete.lua
@@ -1,5 +1,4 @@
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
     sources = {
@@ -9,18 +8,8 @@ cmp.setup({
     },
 
     mapping = cmp.mapping.preset.insert({
-        -- `Enter` key to confirm completion
         ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-        -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
-
-        -- Navigate between snippet placeholder
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-        -- Scroll up and down in the completion documentation
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-a>'] = cmp.mapping.abort(),
     })
 })
