56469875e054eac4bf33aadba2a0c1f98f35fd4c
Giovanni Maria Cusaro <gcusaro@me.com>
2023-10-14 21:46:59 +0200

    Clean code

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -6,8 +6,6 @@ require('gitsigns').setup({
         topdelete = { text = "‾" },
         changedelete = { text = "~" },
         untracked = { text = "¦" }
-        -- add          = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        -- untracked    = { hl = 'GitSignsAdd', text = '¦', numhl='GitSignsAddNr', linehl='GitSignsAddLn' },
     },
     current_line_blame_formatter = '<summary> | <author_time:%d-%m-%Y> - <author>',
     current_line_blame_opts = {
@@ -15,8 +13,6 @@ require('gitsigns').setup({
     },
 })
 
-
 local signs = package.loaded.gitsigns
 local opts = { silent = true, noremap = true }
-
 vim.keymap.set("n", "<leader>gi", signs.toggle_current_line_blame, opts)
