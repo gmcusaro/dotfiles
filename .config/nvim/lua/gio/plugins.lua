341c2d3f4b7f82ba9be22022686c82cf9e746b29
Giovanni Maria Cusaro <gcusaro@me.com>
2023-11-04 14:51:54 +0100

    clean code

--- .config/nvim/lua/gio/plugins.lua
+++ .config/nvim/lua/gio/plugins.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -31,6 +31,7 @@ require('lazy').setup({
             "meuter/lualine-so-fancy.nvim",
         }
     },
+
     { "ur4ltz/surround.nvim" },
     { "numToStr/Comment.nvim" },
     { "folke/zen-mode.nvim" },
