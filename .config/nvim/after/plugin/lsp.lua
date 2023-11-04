bf692e6e7ee65dae408d72087f9319a3a4533c6d
Giovanni Maria Cusaro <gcusaro@me.com>
2023-11-04 14:50:49 +0100

    refactor code

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,7 +1,7 @@
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+local lsp_zero = require('lsp-zero')
+local lsp_config = require('lspconfig')
 
-lsp.set_preferences({
+lsp_zero.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
         error = 'E',
@@ -11,7 +11,7 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(_, bufnr)
+lsp_zero.on_attach(function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -24,6 +24,7 @@ lsp.on_attach(function(_, bufnr)
     vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 end)
 
+lsp_zero.setup()
 
 -- local lua_opts = lsp.nvim_lua_ls()
 -- lsp.configure('lua_ls', {
@@ -36,25 +37,43 @@ end)
 --     }
 -- })
 
-local lua_opts = lsp.nvim_lua_ls()
-require('lspconfig').lua_ls.setup(lua_opts)
-
--- lsp.configure('tsserver', {
---     -- single_file_support = false,
---     settings = {
---         completions = {
---             completeFunctionCalls = true
---         }
---     }
--- })
-
-require('lspconfig').tsserver.setup({
-  single_file_support = false,
-  on_init = function(client)
-    -- disable formatting capabilities
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentFormattingRangeProvider = false
-  end,
+local lua_opts = lsp_zero.nvim_lua_ls({
+    single_file_support = false
 })
 
-lsp.setup()
+lsp_config.lua_ls.setup(lua_opts)
+
+lsp_config.tsserver.setup({
+    single_file_support = false,
+    settings = {
+        completions = {
+            completeFunctionCalls = true
+        }
+    },
+    on_init = function(client)
+        -- disable formatting capabilities
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentFormattingRangeProvider = false
+    end,
+})
+
+-- Swift
+ local swift_lsp = vim.api.nvim_create_augroup("swift_lsp", { clear = true })
+
+ vim.api.nvim_create_autocmd("FileType", {
+ 	pattern = { "swift" },
+ 	callback = function()
+ 		local root_dir = vim.fs.dirname(vim.fs.find({
+ 			"Package.swift",
+ 			".git",
+ 		}, { upward = true })[1])
+ 		local client = vim.lsp.start({
+ 			name = "sourcekit-lsp",
+ 			cmd = { "sourcekit-lsp" },
+ 			root_dir = root_dir,
+ 		})
+ 		vim.lsp.buf_attach_client(0, client)
+ 	end,
+ 	group = swift_lsp,
+ })
+
