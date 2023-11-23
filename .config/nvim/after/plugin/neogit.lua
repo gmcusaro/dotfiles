d978738d880b9a3e692917a9af9ef87ef1f042a4
Giovanni Maria Cusaro <gcusaro@me.com>
2023-11-23 10:01:47 +0100

    fix kind mode

--- .config/nvim/after/plugin/neogit.lua
+++ .config/nvim/after/plugin/neogit.lua
@@ -4,16 +4,14 @@ neogit.setup {
     integrations = {
         diffview = true
     },
-    -- disable_hint = true,
     disable_commit_confirmation = true,
-    kind = "split"
+    auto_show_console = false,
+    kind = "split",
+    commit_editor = {
+        kind = "split",
+    },
 }
 
 local opts = { silent = true, noremap = true }
-local function opensplit()
-    neogit.open({ kind = "split" })
-end
-
-vim.keymap.set("n", "<leader>gs", opensplit, opts)
-vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>", opts)
+vim.keymap.set("n", "<leader>gs", neogit.open, opts)
 vim.keymap.set("n", "<leader>gf", "<cmd>!git fetch --all<CR>", opts)
