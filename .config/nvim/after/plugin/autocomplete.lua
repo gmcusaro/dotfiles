bcd020beb1b9332efaac2d09bb6f626bfeecfb45
Giovanni Maria Cusaro <gcusaro@me.com>
2023-11-25 10:01:04 +0100

    replace next/prev item selection

--- .config/nvim/after/plugin/autocomplete.lua
+++ .config/nvim/after/plugin/autocomplete.lua
@@ -4,16 +4,19 @@ local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
     sources = {
         { name = 'nvim_lsp' },
-        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'path' },
         { name = 'buffer' },
     },
 
     mapping = cmp.mapping.preset.insert({
         ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-        ['<C-a>'] = cmp.mapping.abort(),
-
-        ['<Tab>'] = cmp_action.tab_complete(),
-        ['<S-Tab>'] = cmp_action.select_prev_or_fallback()
+        ['<C-a>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item()
     })
 })
