9ae184575692ded03a29652f6bc26d7ea449854b
Giovanni Maria Cusaro <gcusaro@me.com>
2024-05-16 13:59:38 +0200

    Add features

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -7,6 +7,7 @@ return {
         config = function()
             require("catppuccin").setup({
                 flavour = 'mocha',
+                -- transparent_background = true,
                 color_overrides = {
                     mocha = {
                         base = '#1e2030',
