93d3ba01823e2817cdae191dd12a75be67b180a0
Giovanni Maria Cusaro <gcusaro@me.com>
2024-09-12 20:03:08 +0200

    edited keymaps

--- .config/nvim/lua/plugins/closebuffer.lua
+++ .config/nvim/lua/plugins/closebuffer.lua
@@ -2,7 +2,7 @@ return {
     "Asheq/close-buffers.vim",
     lazy = false,
     keys = {
-        vim.keymap.set("n", "<leader>b", ":Bdelete hidden<CR>", { silent = true }),
-        vim.keymap.set("n", "<leader>t", ":Bdelete this<CR>", { silent = true })
+        vim.keymap.set("n", "<leader>o", ":Bdelete hidden<CR>", { silent = true }),
+        vim.keymap.set("n", "<leader>b", ":Bdelete this<CR>", { silent = true })
     }
 }
