ceb77be1dd0ecbeda0ba7b63b53b2fa6e7607e16
Giovanni Maria Cusaro <gcusaro@me.com>
2024-09-12 20:02:25 +0200

    added keymaps

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,17 +1,26 @@
-local config = function()
-    require("luasnip.loaders.from_vscode").lazy_load()
-
-    require("luasnip").setup({
-        history = true,
-        updateevents = "TextChanged,TextChangedI",
-    })
-end
-
 return {
     "L3MON4D3/LuaSnip",
     dependencies = {
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets"
     },
-    config = config
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip").setup({
+            history = true,
+            updateevents = "TextChanged,TextChangedI",
+        })
+
+        local ls = require("luasnip")
+        local silent = { silent = true }
+
+        vim.keymap.set({ "i" }, "<C-k>", function() ls.expand() end, silent)
+        vim.keymap.set({ "i", "s" }, "<C-h>", function() ls.jump(-1) end, silent)
+        vim.keymap.set({ "i", "s" }, "<C-l>", function() ls.jump(1) end, silent)
+        -- vim.keymap.set({ "i", "s" }, "<C-e>", function()
+        --     if ls.choice_active() then
+        --         ls.change_choice(1)
+        --     end
+        -- end, silent)
+    end
 }
