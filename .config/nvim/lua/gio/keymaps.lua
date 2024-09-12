f3ed7dcff9288d69a98ddc50e90821a00e9ffca4
Giovanni Maria Cusaro <gcusaro@me.com>
2024-09-12 20:12:57 +0200

    Setted and update several keys

--- .config/nvim/lua/gio/keymaps.lua
+++ .config/nvim/lua/gio/keymaps.lua
@@ -1,46 +1,51 @@
 local opts = { silent = true, noremap = true }
-local keymap = vim.keymap
+local map = vim.keymap
 
-keymap.set({"n", "v"}, "<leader>", "", opts)
-keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
-keymap.set("n", "<leader>a", "ggVG", opts) -- Select all
-keymap.set("n", "<C-s>", ":set scrollbind! scrollbind?<CR>", opts)
-keymap.set("n", "U", "<C-r>", opts) -- U for redo
-keymap.set("x", "p",'"_dP', opts) -- Fix paste
-keymap.set("v", "<BS>", "\"_d", opts) -- Backspace delete visual mode
-keymap.set("n", "x", '"_x') -- Do not yank with x
-keymap.set("n", "<leader>p", "\"_dP", opts) --replace  selected text with default register without yanking it
+-- vim.keymap.set("i", "jk", "<Esc>", opts)
+vim.keymap.set("n", "<Esc>", function()
+    vim.fn.setreg("/", "")
+    vim.cmd("nohlsearch")
+    vim.cmd("echo ''")
+end, opts)
 
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
-keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
-keymap.set("n", "J", "mzJ`z")
+-- map.set("n", "<C-s>", "<cmd>scrollbind! scrvllbind?<CR>", opts)
+map.set("n", "<leader>a", "ggVG", opts)     -- Select all
+map.set("n", "F", "^", { noremap = false }) -- First word
+map.set("n", "E", "$", { noremap = false }) -- End line
+map.set("v", "K", ":m '<-2<CR>gv=gv", opts) -- Move line up
+map.set("v", "J", ":m '>+1<CR>gv=gv", opts) -- Move line down
+map.set("n", "U", "<C-r>", opts)            -- U for redo
+map.set("v", "<BS>", '"_d', opts)           -- Delete without yank in visual mode
+map.set("n", "x", '"_x')                    -- Do not yank with x
+map.set("x", "p", '"_dP', opts)             -- Do not yank the replaced text after pasting
+map.set("n", "gp", [[`[v`]']], opts)        -- Select just pasted
 
 -- Bring search result to midscreen
-keymap.set("n", "n", "nzzzv")
-keymap.set("n", "N", "Nzzzv")
+map.set("n", "n", "nzzzv")
+map.set("n", "N", "Nzzzv")
 
 -- Move in wrap text
-keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "gk", [[?^##\+<CR>]], { buffer = true, silent = true })
+vim.keymap.set("n", "gj", [[/^##\+<CR>]], { buffer = true, silent = true })
 
 -- Navigation/Split/Resize
-keymap.set("n", "sh", "<C-w>h", opts)
-keymap.set("n", "sj", "<C-w>j", opts)
-keymap.set("n", "sk", "<C-w>k", opts)
-keymap.set("n", "sl", "<C-w>l", opts)
-
-keymap.set("n", "vs", ":vsplit<CR>", opts)
-keymap.set("n", "os", ":split<CR>", opts)
-keymap.set("n", "es", "<C-w>=", opts)
-keymap.set("n", "cs", ":close<CR>", opts)
-
-keymap.set("n", "<leader>+", ":vertical resize +4<CR>", opts)
-keymap.set("n", "<leader>-", ":vertical resize -4<CR>", opts)
+map.set("n", "wh", "<C-w>h", opts)
+map.set("n", "wj", "<C-w>j", opts)
+map.set("n", "wk", "<C-w>k", opts)
+map.set("n", "wl", "<C-w>l", opts)
+map.set("n", "vs", ":vsplit<CR>", opts)
+map.set("n", "os", ":split<CR>", opts)
+map.set("n", "es", "<C-w>=", opts)
+map.set("n", "cs", ":close<CR>", opts)
+map.set("n", "<leader>+", ":vertical resize +4<CR>", opts)
+map.set("n", "<leader>-", ":vertical resize -4<CR>", opts)
 
 -- Buffer
-keymap.set("n", "<TAB>", ":bnext<CR>", opts)
-keymap.set("n", "<S-TAB>", ":bprevious<CR>", opts)
+map.set("n", "<TAB>", ":bnext<CR>", opts)
+map.set("n", "<S-TAB>", ":bprevious<CR>", opts)
 
 -- Indenting
-keymap.set("v", "<TAB>", ">gv")
-keymap.set("v", "<S-TAB>", "<gv")
+map.set("v", "<TAB>", ">gv")
+map.set("v", "<S-TAB>", "<gv")
