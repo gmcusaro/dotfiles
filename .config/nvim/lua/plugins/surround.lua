11133f8e7e73cc0d80ba6e25b5d7eeb8c6d34d52
Giovanni Maria Cusaro <gcusaro@me.com>
2024-09-12 20:01:22 +0200

    testing alternative

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,16 +1,53 @@
+-- return {
+--     "ur4ltz/surround.nvim",
+--     lazy = false,
+--     opts = {
+--         prompt = false,
+--         mappings_style = "sandwich",
+--         pairs = {
+--             nestable = {
+--                 t = { "(", ")" },
+--                 q = { "[", "]" },
+--                 g = { "{", "}" },
+--                 a = { "<", ">" }
+--             },
+--             linear = {
+--                 q = { "'", "'" },
+--                 d = { '"', '"' },
+--             }
+--         },
+--         prefix = 's'
+--     }
+-- }
+
+-- return {
+--     "echasnovski/mini.nvim",
+--     version = false,
+--     opts = {
+--         mappings = {
+--             add = "gsa",
+--             delete = "gsd",
+--             find = "gsf",
+--             find_left = "gsF",
+--             highlight = "gsh",
+--             replace = "gsr",
+--             update_n_lines = "gsn",
+--         }
+--     }
+-- }
+
+
 return {
-    "ur4ltz/surround.nvim",
-    lazy = false,
+    "echasnovski/mini.surround",
     opts = {
-        prompt = false,
-        mappings_style = "sandwich",
-        pairs = {
-            nestable = { t = { "(", ")" }, q = { "[", "]" }, g = { "{", "}" }, a = { "<", ">" } },
-            linear = {
-                q = { "'", "'" },
-                d = { '"', '"' },
-            }
-        },
-        prefix = "s"
-    }
-}
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "sr",
+        update_n_lines = "sn",
+      },
+    },
+  }
